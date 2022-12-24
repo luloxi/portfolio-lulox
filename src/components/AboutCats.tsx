46fa3378e37bb881ed9c583e9162562be496b8b7
Lulox <ligma@duck.com>
2022-12-24 18:46:25 -0300

    Fixed bug that prevented deploying

--- src/components/AboutCats.tsx
+++ src/components/AboutCats.tsx
@@ -21,34 +21,7 @@ export default function Hero() {
         marginBottom={6}
         direction={{ base: "column", md: "row" }}
       >
-        border="1px solid red"
         <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-          {/* <Heading
-            lineHeight={1.1}
-            fontWeight={600}
-            fontSize={{ base: "4xl", sm: "5xl", lg: "6xl" }}
-          >
-            <Text
-              as={"span"}
-              position={"relative"}
-              _after={{
-                content: "''",
-                width: "full",
-                height: "30%",
-                position: "absolute",
-                bottom: 1,
-                left: 0,
-                bg: "blue.400",
-                zIndex: -1,
-              }}
-            >
-              Hola,
-            </Text>
-            <br />
-            <Text as={"span"} color={"blue.400"}>
-              soy Luciano
-            </Text>
-          </Heading> */}
           <Text color={useColorModeValue("gray.700", "gray.300")}>
             Curiosidades sobre mi... ¡me encantan los gatos! Disfruto de hacer
             yoga, prefiero los ventiladores a los aires acondicionados, a veces
@@ -64,38 +37,6 @@ export default function Hero() {
             h={"100%"}
             src={"/about/horizontal.jpg"}
           />
-          {/* <Stack
-            spacing={{ base: 4, sm: 6 }}
-            justifyContent={"center"}
-            direction={{ base: "row", sm: "row" }}
-          >
-            <Link href="proyectos">
-              <Button
-                rounded={"full"}
-                size={"lg"}
-                fontWeight={"normal"}
-                px={6}
-                color={useColorModeValue("gray.800", "gray.100")}
-                bg={useColorModeValue("blue.300", "blue.700")}
-                _hover={{ bg: useColorModeValue("blue.200", "blue.600") }}
-              >
-                Mis proyectos
-              </Button>
-            </Link>
-            <Link href="sobre-mi">
-              <Button
-                rounded={"full"}
-                size={"lg"}
-                fontWeight={"normal"}
-                px={6}
-                color={useColorModeValue("gray.800", "gray.100")}
-                bg={useColorModeValue("gray.400", "gray.700")}
-                _hover={{ bg: useColorModeValue("gray.300", "gray.600") }}
-              >
-                Sobre mí
-              </Button>
-            </Link>
-          </Stack> */}
         </Stack>
         <Flex
           flex={1}
