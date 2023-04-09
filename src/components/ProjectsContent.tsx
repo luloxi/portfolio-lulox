ff2560863f90f78acc965272e039a86187184cca
Lulox <ligma@duck.com>
2023-04-09 13:10:04 -0300

    Added Tuse to projects

--- src/components/ProjectsContent.tsx
+++ src/components/ProjectsContent.tsx
@@ -56,11 +56,7 @@ export default function ProjectsContent() {
               mb={6}
               pos={"relative"}
             >
-              <Image
-                alt="Ethernaut with Hardhat"
-                src={"/projects/ethernaut.png"}
-                layout={"fill"}
-              />
+              <Image alt="Tuse" src={"/projects/tuse.jpg"} layout={"fill"} />
             </Box>
             <Stack>
               <Text
@@ -70,28 +66,42 @@ export default function ProjectsContent() {
                 fontSize={"sm"}
                 letterSpacing={1.1}
               >
-                Smart contracts
+                Dapps
               </Text>
               <Heading
                 color={useColorModeValue("gray.700", "white")}
                 fontSize={"2xl"}
                 fontFamily={"body"}
               >
-                Ethernaut with Hardhat
+                Tuse
               </Heading>
               <Text color={"gray.500"}>
-                Repo solving most Ethernaut security challenges, most inspired
-                in famous security breaches and known failures and
-                vulnerabilities in the Solidity language and EVM ecosystem.
+                Descentralized mutual fund dApp, governed by a DAO, one vote per
+                NFT, which changes dynamnically depending if that NFT has funds
+                associated with it or not, and a Vault.
               </Text>
               <Center>
+                <Link href="https://tuse.vercel.app/" target={"_blank"}>
+                  <Button
+                    rounded={"full"}
+                    size={"lg"}
+                    fontWeight={"normal"}
+                    px={6}
+                    color={useColorModeValue("white", "gray.100")}
+                    bg={useColorModeValue("orange.500", "orange.700")}
+                    _hover={{ bg: useColorModeValue("blue.400", "blue.600") }}
+                  >
+                    Web
+                  </Button>
+                </Link>
                 <Link
-                  href="https://github.com/luloxi/ethernaut-with-hardhat"
+                  href="https://github.com/manuel-salvador/hackathon-tuse"
                   target={"_blank"}
                 >
                   <Button
                     rounded={"full"}
                     size={"lg"}
+                    marginLeft={"5"}
                     fontWeight={"normal"}
                     px={6}
                     color={useColorModeValue("white", "gray.100")}
@@ -146,7 +156,7 @@ export default function ProjectsContent() {
                 fontSize={"2xl"}
                 fontFamily={"body"}
               >
-                Sportsbook - UNDER CONSTRUCTION
+                Sportsbook
               </Heading>
               <Text color={"gray.500"}>
                 Social dApp for connecting local teams of a given sport, that
@@ -160,6 +170,75 @@ export default function ProjectsContent() {
               <Center>
                 <Link
                   href="https://github.com/luloxi/sportsbook-dapp"
+                  target={"_blank"}
+                >
+                  <Button
+                    rounded={"full"}
+                    size={"lg"}
+                    fontWeight={"normal"}
+                    px={6}
+                    color={useColorModeValue("white", "gray.100")}
+                    bg={useColorModeValue("blue.500", "blue.700")}
+                    _hover={{ bg: useColorModeValue("blue.400", "blue.600") }}
+                  >
+                    View on GitHub
+                  </Button>
+                </Link>
+              </Center>
+            </Stack>
+          </Box>
+        </GridItem>
+        {/* Third card */}
+        <GridItem colSpan={colSpan}>
+          <Box
+            maxW={"445px"}
+            _hover={{ bg: useColorModeValue("gray.100", "gray.800") }}
+            bg={useColorModeValue("white", "gray.900")}
+            boxShadow={"2xl"}
+            m={4}
+            rounded={"md"}
+            p={6}
+            overflow={"hidden"}
+          >
+            <Box
+              h={"230px"}
+              bg={"gray.100"}
+              mt={-6}
+              mx={-6}
+              mb={6}
+              pos={"relative"}
+            >
+              <Image
+                alt="Ethernaut with Hardhat"
+                src={"/projects/ethernaut.png"}
+                layout={"fill"}
+              />
+            </Box>
+            <Stack>
+              <Text
+                color={"green.500"}
+                textTransform={"uppercase"}
+                fontWeight={800}
+                fontSize={"sm"}
+                letterSpacing={1.1}
+              >
+                Smart contracts
+              </Text>
+              <Heading
+                color={useColorModeValue("gray.700", "white")}
+                fontSize={"2xl"}
+                fontFamily={"body"}
+              >
+                Ethernaut with Hardhat
+              </Heading>
+              <Text color={"gray.500"}>
+                Repo solving most Ethernaut security challenges, most inspired
+                in famous security breaches and known failures and
+                vulnerabilities in the Solidity language and EVM ecosystem.
+              </Text>
+              <Center>
+                <Link
+                  href="https://github.com/luloxi/ethernaut-with-hardhat"
                   target={"_blank"}
                 >
                   <Button
